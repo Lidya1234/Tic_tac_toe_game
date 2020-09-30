40cb75402b939da36a37185b30edf5b3eeced099
Lidya1234 <lidya94gher@gmail.com>
2020-09-30 19:54:30 +0300

    change double quotes to single quotes

--- bin/main.rb
+++ bin/main.rb
@@ -1,38 +1,39 @@
 #!/usr/bin/env ruby
 
-player1 = ""
-player2 = ""
-puts "Enter name of player1 "
+player1 = ''
+player2 = ''
+choice = ''
+puts 'Enter name of first player'
 valid = true
 loop do
   player1 = gets.chomp
 
   break if valid == true
 
-  puts "enter valid name"
+  puts 'Enter valid name'
 end
-puts "Enter name of player2"
+puts 'Enter name of second player'
 loop do
   player2 = gets.chomp
   break if valid == true
 
-  puts "enter valid name"
+  puts 'Enter valid name'
 end
 
 boardisfull = false
 choicevalid = true
 until boardisfull do
-  puts "Enter choice of #{player1}"
+  puts "Enter your choice  #{player1}"
   loop do
     choice = gets.chomp.to_i
     break if choicevalid == true
   end
-  puts "#{player1} your choice is displayed"
-  puts "Enter choice of #{player2}"
+  puts "#{player1} you chose #{choice} "
+  puts "Enter your choice  #{player2}"
   loop do
     choice = gets.chomp.to_i
     break if choicevalid == true
   end
-  puts "#{player2} your choice is displayed"
+  puts "#{player2} you chose #{choice}"
   boardisfull = true
 end
