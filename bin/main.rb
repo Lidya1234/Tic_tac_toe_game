8afccb5a827bdf40d1d8467dcfc9203f21f4e1e9
Lidya1234 <lidya94gher@gmail.com>
2020-09-30 18:49:01 +0300

    Added content to main.rb file

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,37 @@
 #!/usr/bin/env ruby
-puts 'Hello World!'
+
+
+puts "Enter name of player1 "
+valid=true
+loop do
+
+player1=gets.chomp
+
+break if valid ==true
+puts "enter valid name"
+ end
+puts "Enter name of player2"
+loop do
+
+player2=gets.chomp
+break if valid ==true
+puts "enter valid name"
+end
+
+boardisfull=false
+choicevalid=true
+until boardisfull do
+ puts "Enter choice of player 1"
+ loop do
+
+ choice=gets.chomp.to_i
+break if choicevalid ==true
+ end
+ puts "Enter choice of player2"
+ loop do
+
+ choice=gets.chomp.to_i
+break if choicevalid ==true
+ end
+ boardisfull=true
+end
