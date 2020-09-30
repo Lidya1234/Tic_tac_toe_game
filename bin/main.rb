93e2221684b8805fa70faaedb0346c8eda3bad4e
Lidya1234 <lidya94gher@gmail.com>
2020-09-30 19:37:16 +0300

    add user_input

--- bin/main.rb
+++ bin/main.rb
@@ -1,37 +1,38 @@
 #!/usr/bin/env ruby
 
-
+player1 = ""
+player2 = ""
 puts "Enter name of player1 "
-valid=true
+valid = true
 loop do
+  player1 = gets.chomp
 
-player1=gets.chomp
+  break if valid == true
 
-break if valid ==true
-puts "enter valid name"
- end
+  puts "enter valid name"
+end
 puts "Enter name of player2"
 loop do
+  player2 = gets.chomp
+  break if valid == true
 
-player2=gets.chomp
-break if valid ==true
-puts "enter valid name"
+  puts "enter valid name"
 end
 
-boardisfull=false
-choicevalid=true
+boardisfull = false
+choicevalid = true
 until boardisfull do
- puts "Enter choice of player 1"
- loop do
-
- choice=gets.chomp.to_i
-break if choicevalid ==true
- end
- puts "Enter choice of player2"
- loop do
-
- choice=gets.chomp.to_i
-break if choicevalid ==true
- end
- boardisfull=true
+  puts "Enter choice of #{player1}"
+  loop do
+    choice = gets.chomp.to_i
+    break if choicevalid == true
+  end
+  puts "#{player1} your choice is displayed"
+  puts "Enter choice of #{player2}"
+  loop do
+    choice = gets.chomp.to_i
+    break if choicevalid == true
+  end
+  puts "#{player2} your choice is displayed"
+  boardisfull = true
 end
