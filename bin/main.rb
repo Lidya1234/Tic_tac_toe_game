e7931bdd774f229981592bea66a7a5078a7f14fd
Lidya1234 <lidya94gher@gmail.com>
2020-10-01 15:20:36 +0300

    added the getname and get sign methods

--- bin/main.rb
+++ bin/main.rb
@@ -1,101 +1,103 @@
 #!/usr/bin/env ruby
-board_arr=[" " , " ", " " ," " ," " ," " ," " ," " ," "]
+board_arr = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def board(board_arr)
-
-  puts "#{ board_arr[0]} | #{ board_arr[1]}| #{ board_arr[2]}"
+  puts "#{board_arr[0]} | #{board_arr[1]}| #{board_arr[2]}"
   puts "__|__|__"
-  puts "#{ board_arr[3]} | #{ board_arr[4]}| #{ board_arr[5]}"
+  puts "#{board_arr[3]} | #{board_arr[4]}| #{board_arr[5]}"
   puts "__|__|__"
-  puts "#{ board_arr[6]} | #{ board_arr[7]}| #{ board_arr[8]}"
-
+  puts "#{board_arr[6]} | #{board_arr[7]}| #{board_arr[8]}"
 end
+
 def draw
- board_arr=[" " , " ", " " ," " ," " ," " ," " ," " ," "]
+  board_arr = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 end
+
 def getplayesrs
-  validname =true
-  player =""
-  sign =""
- loop do
-    player= gets.chomp
+  validname = true
+  player = ""
+  sign = ""
+  loop do
+    player = gets.chomp
     break if validname == true
-    "Invalid name:Please enter valid Name" 
-  end 
+
+    "Invalid name:Please enter valid Name"
+  end
   player
 end
 
 def getsign
-  validsign =true
+  validsign = true
   sign = ""
-loop do
+  loop do
     sign = gets.chomp
-    break if validsign ==true
-    "Invalid sign:Please enter valid sign" 
-  end  
+    break if validsign == true
+
+    "Invalid sign:Please enter valid sign"
+  end
   sign
 end
 
 def move(player)
-choicevalid = true
-choice =""
+  choicevalid = true
+  choice = ""
   loop do
     choice = gets.chomp.to_i
- 
+
     break if choicevalid == true
+
     puts 'Invalid move'
-   
-  end 
+  end
   choice
 end
-player1 =""
-player1sign =""
- puts "Enter name of first player"
- player1 = getplayesrs
+player1 = ""
+player1sign = ""
+puts "Enter name of first player"
+player1 = getplayesrs
 
- puts "#{player1} Enter sign"
- player1sign = getsign
+puts "#{player1} Enter sign"
+player1sign = getsign
 
- player12 =""
-player2sign =""
- puts "Enter name of second player"
- player2 = getplayesrs
+player12 = ""
+player2sign = ""
+puts "Enter name of second player"
+player2 = getplayesrs
 
- puts "#{player2} Enter sign"
- player2sign = getsign
- board(board_arr)
+puts "#{player2} Enter sign"
+player2sign = getsign
+board(board_arr)
 
- availablemoves=[1,2,3,4,5,6,7,8,9]
+availablemoves = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 boardisfull = false
 
 winningmove = false
-turn =1
+turn = 1
 until boardisfull
 
- if turn % 2 == 1
- 
-  print "available moves #{availablemoves}"#updates after every move
-  puts "select a move  #{player1}"
- choice = move(player1)
-  puts "#{player1} you chose #{choice} "
-  if winningmove
-    puts "Winner #{player1}"
-    break
-  end
-  turn +=1
-else
+  if turn % 2 == 1
 
-   print "available moves #{availablemoves}"#updates after every move
-puts "select a move  #{player2}"
- choice = move(player2)
-  puts "#{player2} you chose #{choice} "
-  if winningmove
-    puts "Winner #{player2}"
-    break
+    print "available moves #{availablemoves}" # updates after every move
+    puts "select a move  #{player1}"
+    choice = move(player1)
+    puts "#{player1} you chose #{choice} "
+    if winningmove
+      puts "Winner #{player1}"
+      break
+    end
+    turn += 1
+  else
+
+    print "available moves #{availablemoves}" # updates after every move
+    puts "select a move  #{player2}"
+    choice = move(player2)
+    puts "#{player2} you chose #{choice} "
+    if winningmove
+      puts "Winner #{player2}"
+      break
+    end
+    turn += 1
   end
-  turn += 1
+  if turn == 5
+    boardisfull = true
+  end
 end
-if turn == 5
-boardisfull = true
-end
-end
-board_arr =draw
+board_arr = draw
